d422b3edc3d662d08328826302ae0d799ac6d791
kdnvt <leowuleo@gmail.com>
2022-03-12 16:31:15 +0800

    Receive data from device driver to user buffer

--- client.c
+++ client.c
@@ -11,7 +11,7 @@ int main()
 {
     long long sz;
 
-    char buf[1];
+    unsigned long long buf[200];
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
@@ -28,20 +28,20 @@ int main()
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, 200 * sizeof(unsigned long long));
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%llu.(size = %lld\n",
+               i, buf[0], sz);
     }
 
     for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, 200 * sizeof(unsigned long long));
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%llu.(size = %lld\n",
+               i, buf[0], sz);
     }
 
     close(fd);
