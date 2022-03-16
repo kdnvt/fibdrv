a6247ad49c9e56d16fdd5ad1a278c256b7a31e09
kdnvt <leowuleo@gmail.com>
2022-03-16 21:31:46 +0800

    Make client.c output multiple runs of data

--- client.c
+++ client.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
+#define RUNS 50
+#define OFFSET 100
 
 int bn_to_string(unsigned long long *bn, int bn_len, char **str_ptr);
 
@@ -18,7 +20,7 @@ int main()
 
     unsigned long long buf[200];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    int offset = OFFSET; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -30,33 +32,48 @@ int main()
         sz = write(fd, write_buf, strlen(write_buf));
         printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
-
+    int kt[OFFSET][RUNS] = {};
+    unsigned long ut[OFFSET][RUNS] = {};
+    int k_to_ut[OFFSET][RUNS] = {};
+    unsigned long total[OFFSET][RUNS] = {};
     FILE *kt_fp = fopen("ktime.txt", "w");
     FILE *k_to_u_fp = fopen("k_to_utime.txt", "w");
     FILE *ut_fp = fopen("utime.txt", "w");
     FILE *total_fp = fopen("total_time.txt", "w");
-    for (int i = 0; i <= 100; i++) {
-        lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 200 * sizeof(unsigned long long));
-        char *str;
-        struct timespec tp_start, tp_end;
-        clock_gettime(CLOCK_MONOTONIC, &tp_start);
-        bn_to_string(buf, sz / sizeof(*buf), &str);
-        clock_gettime(CLOCK_MONOTONIC, &tp_end);
-        unsigned long ut = tp_end.tv_nsec - tp_start.tv_nsec;
-        fprintf(ut_fp, "%d %lu\n", i, ut);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%s.\n",
-               i, str);
-        lseek(fd, 0, SEEK_SET);
-        int kt = write(fd, write_buf, strlen(write_buf));
-        fprintf(kt_fp, "%d %d\n", i, kt);
-        lseek(fd, 1, SEEK_SET);
-        int k_to_ut = write(fd, write_buf, strlen(write_buf));
-        fprintf(k_to_u_fp, "%d %d\n", i, k_to_ut);
-        unsigned long total = kt + k_to_ut + ut;
-        fprintf(total_fp, "%d %lu\n", i, total);
+    for (int j = 0; j < RUNS; j++) {
+        for (int i = 0; i < offset; i++) {
+            lseek(fd, i, SEEK_SET);
+            sz = read(fd, buf, 200 * sizeof(unsigned long long));
+            char *str;
+            struct timespec tp_start, tp_end;
+            clock_gettime(CLOCK_MONOTONIC, &tp_start);
+            bn_to_string(buf, sz / sizeof(*buf), &str);
+            clock_gettime(CLOCK_MONOTONIC, &tp_end);
+            free(str);
+            ut[i][j] = tp_end.tv_nsec - tp_start.tv_nsec;
+
+            printf("Reading from " FIB_DEV
+                   " at offset %d, returned the sequence "
+                   "%s.\n",
+                   i, str);
+            lseek(fd, 0, SEEK_SET);
+            kt[i][j] = write(fd, write_buf, strlen(write_buf));
+            lseek(fd, 1, SEEK_SET);
+            k_to_ut[i][j] = write(fd, write_buf, strlen(write_buf));
+            total[i][j] = kt[i][j] + k_to_ut[i][j] + ut[i][j];
+        }
+    }
+    for (int i = 0; i < offset; i++) {
+        for (int j = 0; j < RUNS; j++) {
+            fprintf(ut_fp, "%lu ", ut[i][j]);
+            fprintf(kt_fp, "%d ", kt[i][j]);
+            fprintf(k_to_u_fp, "%d ", k_to_ut[i][j]);
+            fprintf(total_fp, "%lu ", total[i][j]);
+        }
+        fprintf(kt_fp, "\n");
+        fprintf(ut_fp, "\n");
+        fprintf(k_to_u_fp, "\n");
+        fprintf(total_fp, "\n");
     }
     fclose(ut_fp);
     fclose(kt_fp);
@@ -72,8 +89,6 @@ int main()
                "%s.\n",
                i, str);
     }
-
-    // bn_to_string(buf)
 
     close(fd);
     return 0;
